7135dab8f9b7ccf1d1f988136aadae3681cff2ae
Caged <justin@labratrevenge.com>
2010-10-24 14:04:14 -0700

    add basic site controller for mostly static pages

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,5 @@
+class SiteController < ApplicationController
+  def about
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Pdxpatrol::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  get '/about', :to => 'site#about'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
