06b8e32449a43e2fca9ed10c1b235d6561286170
Caged <justin@labratrevenge.com>
2011-03-10 15:11:33 -0800

    cleanup crime model reporting code

--- app/models/crime/reporting.rb
+++ app/models/crime/reporting.rb
@@ -8,12 +8,7 @@ class Crime
       var ra = this.reported_at;
       emit({month: ra.getMonth(), year: ra.getFullYear()}, {count:1, date: ra}) 
     }"
-    Crime.collection.map_reduce(map, simple_date_reduce, 
-      :query => {
-        :reported_at => {
-          '$lt' => finish, 
-          '$gte' => start
-        }}, :out => 'monthly_citywide_totals_report_' + start.year.to_s)
+    query_totals_for('monthly', start, finish, map)
   end
   
   def self.weekly_totals_between(start, finish = Time.now)
@@ -21,15 +16,18 @@ class Crime
       var week = getWeek(this.reported_at)
       emit({week: week, year: this.reported_at.getFullYear()}, {count:1, date: this.reported_at}) 
     }"
-
-    Crime.collection.map_reduce(map, simple_date_reduce, 
-      :query => {:reported_at => {
-        '$lt' => finish, 
-        '$gte' => start
-      }}, :out => 'weekly_citywide_totals_report_' + start.year.to_s)
+    query_totals_for('weekly', start, finish, map)
   end
   
   private
+  def self.query_totals_for(scope, start, finish, map)
+    collection.map_reduce(map, simple_date_reduce, 
+      :query => {:reported_at => {
+        '$lt' => finish, 
+        '$gte' => start
+      }}, :out => "#{scope}_citywide_totals_report_#{start.year.to_s}")
+  end
+  
   def self.simple_date_reduce
     <<-JS
       function(key, vals) {
