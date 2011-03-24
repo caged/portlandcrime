2bb74faace935dfb5b4edd6b42bc728aa068b257
Caged <justin@labratrevenge.com>
2011-03-24 09:31:29 -0700

    a little more work on d3 conversion.  my grasp on scales is still shaky at best

--- app/coffee/trends.coffee
+++ app/coffee/trends.coffee
@@ -5,34 +5,52 @@ $ ->
       mlabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
       curYear = new Date().getUTCFullYear()
       p = 20
-      w = $('#trends').width() - .5 - p
-      h = 200 - .5 - p
+      nweeks = weeks[0].values.length
+      w = $('#trends').width() -  p * 10
+      h = 200 -  p
       wmax = d3.max weeks, (d) -> d3.max d.values, (e) -> e.value
-      x0 = d3.scale.ordinal().domain(d3.range weeks[0].values.length).rangeBands [0, w], 0.5
-      x1 = d3.scale.ordinal().domain(d3.range weeks[0].values.length).rangeBands [0, w], 0.5
-      
+      x0 = d3.scale.ordinal().domain(d3.range nweeks).rangeBands [0, w], 0.5
+      x1 = d3.scale.ordinal().domain(d3.range nweeks).rangeBands [0, x0.rangeBand()]
       y = d3.scale.linear().domain([0, wmax]).range [0, h]
       
       vis = d3.select('#weekly')
         .append('svg:svg')
           .attr('width', w + p)
           .attr('height', h)
+      
+      # rules = vis.selectAll('g.rule')
+      #     .data()
+      #   .enter().append("svg:g")
+      #     .attr("class", "rule")
+      #   
+      # rules.append('svg:line')
+      #   .attr("y1", y)
+      #   .attr("y2", y)
+      #   .attr("x1", 0)
+      #   .attr("x2", w + 1);
           
       layers = vis.selectAll('g.layer')
           .data(weeks)
         .enter().append('svg:g')
           .attr('class', 'layer')
-          .attr('transform', (d, i) -> "translate(#{i * x.rangeBand()},0)")
+          .attr('transform', (d, i) -> "translate(#{x1(i)},0)")
           .attr('fill', (d) -> if d.series == 'prev' then '#00b2ec' else '#cccccc')
             
       bars = layers.selectAll('g.bar')
           .data((d)-> d.values)
         .enter().append('svg:g')
           .attr('class', 'bar')
-          .attr('transform', (d) -> "translate(#{x(d.week)},0)")
+          .attr('transform', (d, i) ->  "translate(#{x0 d.week},0)")
       
       bars.append('svg:rect')
-        .attr('width', 5)
+        .attr('width', 3)
         .attr('x', 0)
         .attr('y', (d) -> h - y d.value)
         .attr('height', (d) -> y d.value)
+        
+      vis.selectAll('text')
+        .data(weeks[0].values)
+      .enter().append('svg:text')
+        .attr("transform", (d, i) -> "translate(#{x0(i)},0)")
+        .attr("text-anchor", "bottom")
+        .text((d)-> d.week)
--- public/javascripts/trends.js
+++ public/javascripts/trends.js
@@ -1,4 +1,4 @@
-/* DO NOT MODIFY. This file was compiled Tue, 22 Mar 2011 20:45:28 GMT from
+/* DO NOT MODIFY. This file was compiled Wed, 23 Mar 2011 16:40:13 GMT from
  * /Users/justin/dev/lrr/rails/portlandcrime/app/coffee/trends.coffee
  */
 
@@ -6,23 +6,27 @@
   $(function() {
     if ($('body[data-path=trends-index]').length !== 0) {
       return $.getJSON('/trends.json', function(data) {
-        var bars, curYear, h, layers, mlabels, months, p, vis, w, weeks, wmax, x, y;
+        var bars, curYear, h, layers, mlabels, months, nweeks, p, rules, vis, w, weeks, wmax, x0, x1, y;
         weeks = data[0], months = data[1];
         mlabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         curYear = new Date().getUTCFullYear();
         p = 20;
-        w = $('#trends').width() - .5 - p;
-        h = 200 - .5 - p;
+        nweeks = weeks[0].values.length;
+        w = $('#trends').width() - p * 10;
+        h = 200 - p;
         wmax = d3.max(weeks, function(d) {
           return d3.max(d.values, function(e) {
             return e.value;
           });
         });
-        x = d3.scale.ordinal().domain(d3.range(weeks[0].values.length)).rangeBands([0, w], 0.5);
+        x0 = d3.scale.ordinal().domain(d3.range(nweeks)).rangeBands([0, w], 0.5);
+        x1 = d3.scale.ordinal().domain(d3.range(nweeks)).rangeBands([0, x0.rangeBand()]);
         y = d3.scale.linear().domain([0, wmax]).range([0, h]);
         vis = d3.select('#weekly').append('svg:svg').attr('width', w + p).attr('height', h);
+        rules = vis.selectAll('g.rule').data(d3.range(wmax)).enter().append("svg:g").attr("class", "rule");
+        rules.append('svg:line').attr("y1", y).attr("y2", y).attr("x1", 0).attr("x2", w + 1);
         layers = vis.selectAll('g.layer').data(weeks).enter().append('svg:g').attr('class', 'layer').attr('transform', function(d, i) {
-          return "translate(" + (i * x.rangeBand()) + ",0)";
+          return "translate(" + (x1(i)) + ",0)";
         }).attr('fill', function(d) {
           if (d.series === 'prev') {
             return '#00b2ec';
@@ -32,13 +36,18 @@
         });
         bars = layers.selectAll('g.bar').data(function(d) {
           return d.values;
-        }).enter().append('svg:g').attr('class', 'bar').attr('transform', function(d) {
-          return "translate(" + (x(d.week)) + ",0)";
+        }).enter().append('svg:g').attr('class', 'bar').attr('transform', function(d, i) {
+          return "translate(" + (x0(d.week)) + ",0)";
         });
-        return bars.append('svg:rect').attr('width', 5).attr('x', 0).attr('y', function(d) {
+        bars.append('svg:rect').attr('width', 3).attr('x', 0).attr('y', function(d) {
           return h - y(d.value);
         }).attr('height', function(d) {
           return y(d.value);
+        });
+        return vis.selectAll('text').data(weeks[0].values).enter().append('svg:text').attr("transform", function(d, i) {
+          return "translate(" + (x0(i)) + ",0)";
+        }).attr("text-anchor", "bottom").text(function(d) {
+          return d.week;
         });
       });
     }
