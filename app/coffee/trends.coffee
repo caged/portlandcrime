4deef566b0bc3a6f15001f1c157675ebe3963e20
Caged <justin@labratrevenge.com>
2011-03-26 11:02:49 -0700

    reuse trend code for drawing monthly chart

--- app/coffee/trends.coffee
+++ app/coffee/trends.coffee
@@ -1,82 +1,102 @@
 $ ->
   if $('body[data-path=trends-index]').length != 0
     $.getJSON '/trends.json', (data) ->
+      # Data
       [weeks, months] = data
       mlabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
       year    = new Date().getUTCFullYear()
-      nweeks  = weeks[0].values.length
-      [pt,pl,pb,pr] = [20,30,20,40]
+      
+      # Demensions, Padding
+      [pt,pl,pb,pr] = [30,30,20,40]
       lblwid  = 60
       w       = $('#trends').width() - (pl + pr + 10)
-      h       = 200 - (pt + pb)
-      wmax    = d3.max weeks, (d) -> d3.max d.values, (e) -> e.value
-      x       = d3.scale.linear().domain([0, wmax]).range [h,0]
-      y0      = d3.scale.ordinal().domain(d3.range nweeks).rangeBands [0, w], 0.5
-      y1      = d3.scale.ordinal().domain(d3.range 2).rangeBands [0, y0.rangeBand()]
+      h       = 230 - (pt + pb)
       
-      vis = d3.select('#weekly')
-        .append('svg:svg')
-          .attr('width', w + (pl + pr))
-          .attr('height', h + pt + pb)
-          .attr('class', 'viz')
-          .append('svg:g')
-            .attr('transform', "translate(#{pl},#{pt})")
+      drawTrend = (el, data) ->
+        # Scales
+        samples = data[0].values.length
+        wmax    = d3.max data, (d) -> d3.max d.values, (e) -> e.value
+        x       = d3.scale.linear().domain([0, wmax]).range [h,0]
+        y0      = d3.scale.ordinal().domain(d3.range samples).rangeBands [0, w], 0.5
+        y1      = d3.scale.ordinal().domain(d3.range 2).rangeBands [0, y0.rangeBand()]
+        labels  = if el == '#monthly' then mlabels else d3.range(samples)
       
-      rules = vis.selectAll('g.rule')
-          .data(x.ticks(10))
-        .enter().append('svg:g')
-          .attr('class', (d) -> if d then null else 'axis')
+        vis = d3.select(el)
+          .append('svg:svg')
+            .attr('width', w + (pl + pr))
+            .attr('height', h + pt + pb)
+            .attr('class', 'viz')
+            .append('svg:g')
+              .attr('transform', "translate(#{pl},#{pt})")
+      
+        g = vis.selectAll('g.bar')
+            .data(data)
+          .enter().append('svg:g')
+            .attr('fill', (d) -> if d.series == 'prev' then '#00b2ec' else '#cccccc')
+            .attr('transform', (d, i) -> "translate(#{y1(i)},0)")
+      
+        g.selectAll('rect')
+          .data((d) -> d.values)
+        .enter().append('svg:rect')
+          .attr('transform', (d,i) -> "translate(#{y0(i) + 0.5},0)")
+          .attr('width', y1.rangeBand() / 2)
+          .attr('height', (d,i) -> h - x(d.value))
+          .attr('y', (d) -> x(d.value))
+        
+        rules = vis.selectAll('g.rule')
+            .data((d) -> if el != '#monthly' then x.ticks(10) else x.ticks(4))
+          .enter().append('svg:g')
+            .attr('class', (d) -> if d then null else 'axis')
           
-      rules.append('svg:line')
-        .attr("y1", x)
-        .attr("y2", x)
-        .attr("x1", 0)
-        .attr("x2", w)
+        rules.append('svg:line')
+          .attr("y1", x)
+          .attr("y2", x)
+          .attr("x1", 0)
+          .attr("x2", w)
         
-      rules.append('svg:text')
-        .attr('y', x)
-        .attr("dy", ".2em")
-        .attr('x', w + 5)
-        .attr('class', 'vlbl')
-        .text((d) -> d)
+        rules.append('svg:text')
+          .attr('y', x)
+          .attr("dy", ".2em")
+          .attr('x', w + 5)
+          .attr('class', 'vlbl')
+          .text((d) -> d)
       
-      vis.selectAll('h.text')
-        .data(d3.range(nweeks))
-      .enter().append('svg:text')
-        .attr('class', (d, i) -> if i % 3 == 0 then 'hlbl' else 'hlbl hide')
-        .attr("transform", (d, i) -> "translate(#{y0(i)},0)")
-        .attr("x", y0.rangeBand() / 2)
-        .attr("y", h + 12)
-        .attr("text-anchor", "middle")
-        .text((d)-> d + 1)
-      
-      g = vis.selectAll('g.bar')
-          .data(weeks)
-        .enter().append('svg:g')
-          .attr('fill', (d) -> if d.series == 'prev' then '#00b2ec' else '#cccccc')
-          .attr('transform', (d, i) -> "translate(#{y1(i)},0)")
-      
-      g.selectAll('rect')
-        .data((d) -> d.values)
-      .enter().append('svg:rect')
-        .attr('transform', (d,i) -> "translate(#{y0(i) + 0.5},0)")
-        .attr('width', y1.rangeBand() / 2)
-        .attr('height', (d,i) -> h - x(d.value))
-        .attr('y', (d) -> x(d.value))
+        vis.selectAll('h.text')
+          .data(labels)
+        .enter().append('svg:text')
+          .attr('class', (d, i) -> if i % 3 != 0 && el != '#monthly' then 'hlbl hide' else 'hlbl')
+          .attr("transform", (d, i) -> "translate(#{y0(i)},0)")
+          .attr("x", y0.rangeBand() / 2)
+          .attr("y", h + 12)
+          .attr("text-anchor", "middle")
+          .text((d)-> if el != '#monthly' then d + 1 else d)
         
-      legend = vis.selectAll('legend')
-        .data([year - 1, year])
+        legend = vis.selectAll('legend')
+          .data([year - 1, year])
         
-      legend.enter().append('svg:circle')
-        .attr('transform', "translate(#{((w - (lblwid * 2)) / 2)}, 0)")
-        .attr('cx', (d, i) -> lblwid * i)
-        .attr('fill', (d) -> if d == year then '#00b2ec' else '#cccccc')
-        .attr('r', 3)
-        .text((d) -> d)
+        legend.enter().append('svg:circle')
+          .attr('transform', "translate(#{((w - (lblwid * 2)) / 2)}, -10)")
+          .attr('cx', (d, i) -> lblwid * i)
+          .attr('fill', (d) -> if d == year then '#00b2ec' else '#cccccc')
+          .attr('r', 3)
+          .text((d) -> d)
       
-      legend.enter().append('svg:text')
-        .attr('transform', "translate(#{((w - (lblwid * 2)) / 2)}, 0)")
-        .attr("x", (d, i) -> 10 + (lblwid * i))
-        .attr('y', 5)
-        .attr('class', 'legend')
-        .text((d) -> d)
+        legend.enter().append('svg:text')
+          .attr('transform', "translate(#{((w - (lblwid * 2)) / 2)}, -10)")
+          .attr("x", (d, i) -> 10 + (lblwid * i))
+          .attr('y', 5)
+          .attr('class', 'legend')
+          .text((d) -> d)
+        
+      drawTrend '#weekly', weeks
+      $(document).bind 'tab.clicked', (event, el) ->
+        el = $(el)
+        sel = $('li a.current').attr 'href'
+        hdr = el.prev 'h1'
+        
+        if sel == '#monthly'
+          hdr.text 'Weekly Trend'
+        else
+          hdr.text 'Monthly Trend'
+          if $('#monthly svg').length == 0
+            drawTrend '#monthly', months
--- public/javascripts/trends.js
+++ public/javascripts/trends.js
@@ -1,4 +1,4 @@
-/* DO NOT MODIFY. This file was compiled Sat, 26 Mar 2011 17:03:39 GMT from
+/* DO NOT MODIFY. This file was compiled Sat, 26 Mar 2011 18:01:41 GMT from
  * /Users/justin/dev/lrr/rails/portlandcrime/app/coffee/trends.coffee
  */
 
@@ -6,80 +6,109 @@
   $(function() {
     if ($('body[data-path=trends-index]').length !== 0) {
       return $.getJSON('/trends.json', function(data) {
-        var g, h, lblwid, legend, mlabels, months, nweeks, pb, pl, pr, pt, rules, vis, w, weeks, wmax, x, y0, y1, year, _ref;
+        var drawTrend, h, lblwid, mlabels, months, pb, pl, pr, pt, w, weeks, year, _ref;
         weeks = data[0], months = data[1];
         mlabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         year = new Date().getUTCFullYear();
-        nweeks = weeks[0].values.length;
-        _ref = [20, 30, 20, 40], pt = _ref[0], pl = _ref[1], pb = _ref[2], pr = _ref[3];
+        _ref = [30, 30, 20, 40], pt = _ref[0], pl = _ref[1], pb = _ref[2], pr = _ref[3];
         lblwid = 60;
         w = $('#trends').width() - (pl + pr + 10);
-        h = 200 - (pt + pb);
-        wmax = d3.max(weeks, function(d) {
-          return d3.max(d.values, function(e) {
-            return e.value;
+        h = 230 - (pt + pb);
+        drawTrend = function(el, data) {
+          var g, labels, legend, rules, samples, vis, wmax, x, y0, y1;
+          samples = data[0].values.length;
+          wmax = d3.max(data, function(d) {
+            return d3.max(d.values, function(e) {
+              return e.value;
+            });
           });
-        });
-        x = d3.scale.linear().domain([0, wmax]).range([h, 0]);
-        y0 = d3.scale.ordinal().domain(d3.range(nweeks)).rangeBands([0, w], 0.5);
-        y1 = d3.scale.ordinal().domain(d3.range(2)).rangeBands([0, y0.rangeBand()]);
-        vis = d3.select('#weekly').append('svg:svg').attr('width', w + (pl + pr)).attr('height', h + pt + pb).attr('class', 'viz').append('svg:g').attr('transform', "translate(" + pl + "," + pt + ")");
-        rules = vis.selectAll('g.rule').data(x.ticks(10)).enter().append('svg:g').attr('class', function(d) {
-          if (d) {
-            return null;
+          x = d3.scale.linear().domain([0, wmax]).range([h, 0]);
+          y0 = d3.scale.ordinal().domain(d3.range(samples)).rangeBands([0, w], 0.5);
+          y1 = d3.scale.ordinal().domain(d3.range(2)).rangeBands([0, y0.rangeBand()]);
+          labels = el === '#monthly' ? mlabels : d3.range(samples);
+          vis = d3.select(el).append('svg:svg').attr('width', w + (pl + pr)).attr('height', h + pt + pb).attr('class', 'viz').append('svg:g').attr('transform', "translate(" + pl + "," + pt + ")");
+          g = vis.selectAll('g.bar').data(data).enter().append('svg:g').attr('fill', function(d) {
+            if (d.series === 'prev') {
+              return '#00b2ec';
+            } else {
+              return '#cccccc';
+            }
+          }).attr('transform', function(d, i) {
+            return "translate(" + (y1(i)) + ",0)";
+          });
+          g.selectAll('rect').data(function(d) {
+            return d.values;
+          }).enter().append('svg:rect').attr('transform', function(d, i) {
+            return "translate(" + (y0(i) + 0.5) + ",0)";
+          }).attr('width', y1.rangeBand() / 2).attr('height', function(d, i) {
+            return h - x(d.value);
+          }).attr('y', function(d) {
+            return x(d.value);
+          });
+          rules = vis.selectAll('g.rule').data(function(d) {
+            if (el !== '#monthly') {
+              return x.ticks(10);
+            } else {
+              return x.ticks(4);
+            }
+          }).enter().append('svg:g').attr('class', function(d) {
+            if (d) {
+              return null;
+            } else {
+              return 'axis';
+            }
+          });
+          rules.append('svg:line').attr("y1", x).attr("y2", x).attr("x1", 0).attr("x2", w);
+          rules.append('svg:text').attr('y', x).attr("dy", ".2em").attr('x', w + 5).attr('class', 'vlbl').text(function(d) {
+            return d;
+          });
+          vis.selectAll('h.text').data(labels).enter().append('svg:text').attr('class', function(d, i) {
+            if (i % 3 !== 0 && el !== '#monthly') {
+              return 'hlbl hide';
+            } else {
+              return 'hlbl';
+            }
+          }).attr("transform", function(d, i) {
+            return "translate(" + (y0(i)) + ",0)";
+          }).attr("x", y0.rangeBand() / 2).attr("y", h + 12).attr("text-anchor", "middle").text(function(d) {
+            if (el !== '#monthly') {
+              return d + 1;
+            } else {
+              return d;
+            }
+          });
+          legend = vis.selectAll('legend').data([year - 1, year]);
+          legend.enter().append('svg:circle').attr('transform', "translate(" + ((w - (lblwid * 2)) / 2) + ", -10)").attr('cx', function(d, i) {
+            return lblwid * i;
+          }).attr('fill', function(d) {
+            if (d === year) {
+              return '#00b2ec';
+            } else {
+              return '#cccccc';
+            }
+          }).attr('r', 3).text(function(d) {
+            return d;
+          });
+          return legend.enter().append('svg:text').attr('transform', "translate(" + ((w - (lblwid * 2)) / 2) + ", -10)").attr("x", function(d, i) {
+            return 10 + (lblwid * i);
+          }).attr('y', 5).attr('class', 'legend').text(function(d) {
+            return d;
+          });
+        };
+        drawTrend('#weekly', weeks);
+        return $(document).bind('tab.clicked', function(event, el) {
+          var hdr, sel;
+          el = $(el);
+          sel = $('li a.current').attr('href');
+          hdr = el.prev('h1');
+          if (sel === '#monthly') {
+            return hdr.text('Weekly Trend');
           } else {
-            return 'axis';
+            hdr.text('Monthly Trend');
+            if ($('#monthly svg').length === 0) {
+              return drawTrend('#monthly', months);
+            }
           }
-        });
-        rules.append('svg:line').attr("y1", x).attr("y2", x).attr("x1", 0).attr("x2", w);
-        rules.append('svg:text').attr('y', x).attr("dy", ".2em").attr('x', w + 5).attr('class', 'vlbl').text(function(d) {
-          return d;
-        });
-        vis.selectAll('h.text').data(d3.range(nweeks)).enter().append('svg:text').attr('class', function(d, i) {
-          if (i % 3 === 0) {
-            return 'hlbl';
-          } else {
-            return 'hlbl hide';
-          }
-        }).attr("transform", function(d, i) {
-          return "translate(" + (y0(i)) + ",0)";
-        }).attr("x", y0.rangeBand() / 2).attr("y", h + 12).attr("text-anchor", "middle").text(function(d) {
-          return d + 1;
-        });
-        g = vis.selectAll('g.bar').data(weeks).enter().append('svg:g').attr('fill', function(d) {
-          if (d.series === 'prev') {
-            return '#00b2ec';
-          } else {
-            return '#cccccc';
-          }
-        }).attr('transform', function(d, i) {
-          return "translate(" + (y1(i)) + ",0)";
-        });
-        g.selectAll('rect').data(function(d) {
-          return d.values;
-        }).enter().append('svg:rect').attr('transform', function(d, i) {
-          return "translate(" + (y0(i) + 0.5) + ",0)";
-        }).attr('width', y1.rangeBand() / 2).attr('height', function(d, i) {
-          return h - x(d.value);
-        }).attr('y', function(d) {
-          return x(d.value);
-        });
-        legend = vis.selectAll('legend').data([year - 1, year]);
-        legend.enter().append('svg:circle').attr('transform', "translate(" + ((w - (lblwid * 2)) / 2) + ", 0)").attr('cx', function(d, i) {
-          return lblwid * i;
-        }).attr('fill', function(d) {
-          if (d === year) {
-            return '#00b2ec';
-          } else {
-            return '#cccccc';
-          }
-        }).attr('r', 3).text(function(d) {
-          return d;
-        });
-        return legend.enter().append('svg:text').attr('transform', "translate(" + ((w - (lblwid * 2)) / 2) + ", 0)").attr("x", function(d, i) {
-          return 10 + (lblwid * i);
-        }).attr('y', 5).attr('class', 'legend').text(function(d) {
-          return d;
         });
       });
     }
